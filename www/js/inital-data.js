a77426139579d0f567bf1772725513fbd925eada
André N. Darcie <andre_darcie@hotmail.com>
2016-08-14 12:55:59 -0300

    Information about the characters

---
+++ www/js/inital-data.js
@@ -0,0 +1,29 @@
+var characters = [
+  {
+    "name": "John",
+    "health": 4,
+    "wound": 0,
+    "hunger": 0,
+    "tired": 0,
+    "isDead": false,
+    "equippedWeapon": 0
+  },
+  {
+    "name": "Mary",
+    "health": 4,
+    "wound": 0,
+    "hunger": 0,
+    "tired": 0,
+    "isDead": false,
+    "equippedWeapon": 0
+  },
+  {
+    "name": "David",
+    "health": 4,
+    "wound": 0,
+    "hunger": 0,
+    "tired": 0,
+    "isDead": false,
+    "equippedWeapon": 0
+  }
+];
