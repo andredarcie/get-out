b940e167821330c133b7e3f40b980cd61e0d2386
André N. Darcie <andrendarcie@gmail.com>
2017-02-05 02:36:43 -0200

    Colors

--- object.js
+++ object.js
@@ -1,8 +1,10 @@
 class Object {
-  constructor({ x, y, width, height}){
+  constructor({ type, x, y, width, height, color }){
+    this.type = type;
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
+    this.color = color;
   }
 }
