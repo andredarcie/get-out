03aac250721006ecdefcd76fe50a44d97d52b3bb
André N. Darcie <andrendarcie@gmail.com>
2018-05-10 20:33:07 -0300

    Add game pages

--- js/main.js
+++ js/main.js
@@ -7,4 +7,15 @@ var GameStates = {
     STATS: 6,
     MAP: 7,
     EVENT: 8
-}
+};
+
+var GamePages = {
+    travelPage: document.getElementById("travel-page"),
+    campPage: document.getElementById("camp-page"),
+    sleepPage: document.getElementById("sleep-page"),
+    huntPage: document.getElementById("hunt-page"),
+    healPage: document.getElementById("heal-page"),
+    statsPage: document.getElementById("stats-page"),
+    mapPage: document.getElementById("map-page"),
+    eventPage: document.getElementById("event-page"),
+};
