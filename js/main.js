d5ba4ac2264f4cc15f8ce2e76e63a79bab36c379
André N. Darcie <andrendarcie@gmail.com>
2018-05-10 21:07:06 -0300

    Add game navigation to states

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ var GameStates = {
     EVENT: 8
 };
 
+var currentState = GameStates.TRAVEL;
+
 var GamePages = {
     travelPage: document.getElementById("travel-page"),
     campPage: document.getElementById("camp-page"),
@@ -19,3 +21,71 @@ var GamePages = {
     mapPage: document.getElementById("map-page"),
     eventPage: document.getElementById("event-page"),
 };
+
+function stateController(){
+    switch(currentState){
+        case GameStates.TRAVEL: 
+            showPage(GamePages.travelPage);
+            hidePage(GamePages.campPage);
+        break;
+        case GameStates.CAMP: 
+            hideAllPages();
+            showPage(GamePages.campPage);
+        break;
+        case GameStates.SLEEP: 
+            showPage(GamePages.sleepPage);
+            hidePage(GamePages.campPage);
+        break;
+        case GameStates.HUNT: 
+            showPage(GamePages.huntPage);
+            hidePage(GamePages.campPage);
+        break;
+        case GameStates.HEAL: 
+            showPage(GamePages.healPage);
+            hidePage(GamePages.campPage);
+        break;
+        case GameStates.STATS: 
+            showPage(GamePages.statsPage);
+            hidePage(GamePages.campPage);
+        break;
+        case GameStates.MAP: 
+            showPage(GamePages.mapPage);
+            hidePage(GamePages.campPage);
+        break;
+        case GameStates.EVENT: 
+            showPage(GamePages.eventPage);
+            hidePage(GamePages.travelPage);
+        break;
+    }
+}
+
+function goToState(state){
+    currentState = state;
+    stateController();
+}
+
+function showPage(page){
+    page.style.display = "block";
+}
+
+function hidePage(page){
+    page.style.display = "none";
+}
+
+function hideAllPages(){
+    hidePage(GamePages.travelPage);
+    hidePage(GamePages.campPage);
+    hidePage(GamePages.sleepPage);
+    hidePage(GamePages.huntPage);
+    hidePage(GamePages.healPage);
+    hidePage(GamePages.statsPage);
+    hidePage(GamePages.mapPage);
+    hidePage(GamePages.eventPage);
+}
+
+gameStart();
+
+function gameStart(){
+    hideAllPages();
+    showPage(GamePages.campPage);
+}
