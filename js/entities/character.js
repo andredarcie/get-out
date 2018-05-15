6a43376a5edb1f52425ffe2be6a5ef25386d15f9
André N. Darcie <andrendarcie@gmail.com>
2018-05-14 21:01:54 -0300

    Add characters

---
+++ js/entities/character.js
@@ -0,0 +1,11 @@
+function Character(name, relationship, thinking, health, wounds, hunger, fatigue, feeling, item){
+    this.name = name,
+    this.relationship = relationship,
+    this.thinking = thinking,
+    this.health = health,
+    this.wounds = wounds,
+    this.hunger = hunger,
+    this.fatigue = fatigue,
+    this.feeling = feeling,
+    this.item = item
+}
--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-var GameStates = {
-    TRAVEL: 1,
-    CAMP: 2,
-    SLEEP: 3,
-    HUNT: 4,
-    HEAL: 5,
-    STATS: 6,
-    MAP: 7,
-    EVENT: 8
-};
-
-var Globals = {
-    currentState: GameStates.TRAVEL,
-    currentDay: 0
-}
-
-var GamePages = {
-    travelPage: document.getElementById("travel-page"),
-    campPage: document.getElementById("camp-page"),
-    sleepPage: document.getElementById("sleep-page"),
-    huntPage: document.getElementById("hunt-page"),
-    healPage: document.getElementById("heal-page"),
-    statsPage: document.getElementById("stats-page"),
-    mapPage: document.getElementById("map-page"),
-    eventPage: document.getElementById("event-page"),
-};
-
 function stateController(){
     switch(Globals.currentState){
         case GameStates.TRAVEL: 
