85570bc25e42c778157c2c4af359cb1200cc0b93
André N. Darcie <andrendarcie@gmail.com>
2018-05-17 00:11:36 -0300

    Add progress bar

--- js/managers/travelManager.js
+++ js/managers/travelManager.js
@@ -1,21 +1,33 @@
 
 var $currentDayField;
+var $nextDayElement;
+var progressBarWidth;
 
 start();
 
 function start(){
+    progressBarWidth = 10;
     $currentDayField = document.getElementById("current-day-field");
+    $nextDayElement = document.getElementById("progress-bar");
 }
 function onClickNextDay(){
 
-    if(calculateEvent()){
-        goToState(GameStates.EVENT);
-    }
+    //if(calculateEvent()){
+        //goToState(GameStates.EVENT);
+    //}
     goToNextDay();
 }
 
 function goToNextDay(){
     Globals.currentDay++;
+
+    progressBarWidth += 10;
+    $nextDayElement.style.width = progressBarWidth + "px";
+
+    if(progressBarWidth > 420){
+        goToState(GameStates.MAP);
+    }
+
     setDay();
 }
 
