d6852bb7025ce2530c68c216d99adb9609b80b70
André N. Darcie <andrendarcie@gmail.com>
2018-05-16 22:36:57 -0300

    Change characters order

--- js/managers/characterManager.js
+++ js/managers/characterManager.js
@@ -1,12 +1,14 @@
 
-start();
+startCharacterManager();
 
-function start(){
+function startCharacterManager(){
+    createAllCharacters();
+}
+
+function createAllCharacters(){
+    Globals.characters.push(new Character("Carter", "you", "I hope to save my family", 10, 0, 0, 0, 0, 0));
+    Globals.characters.push(new Character("Mary", "wife", "We're lost", 10, 0, 0, 0, 0, 0));
     Globals.characters.push(new Character("Alice", "daughter", "I wanted to go home", 10, 0, 0, 0, 0, 0));
     Globals.characters.push(new Character("Issac", "son", "Just leave me alone", 10, 0, 0, 0, 0, 0));
     Globals.characters.push(new Character("Teresa", "grandmother", "I just wanted to die", 10, 0, 0, 0, 0, 0));
-    Globals.characters.push(new Character("Mary", "wife", "We're lost", 10, 0, 0, 0, 0, 0));
-    Globals.characters.push(new Character("Carter", "you", "I hope to save my family", 10, 0, 0, 0, 0, 0));
-
-    console.log(Globals.characters);
 }
