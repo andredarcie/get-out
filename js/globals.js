50c79e6e4dd068b2ac5661be2bb8b43b05b92224
André N. Darcie <andrendarcie@gmail.com>
2018-06-30 11:44:43 -0300

    Add elements in global

--- js/globals.js
+++ js/globals.js
@@ -25,5 +25,55 @@ var Globals = {
         walkingPage: document.getElementById("walking-page"),
         reportPage: document.getElementById("report-page"),
         gameOverPage: document.getElementById("game-over-page"),
+    },
+    pages: {
+        travelPage: {
+            element: document.getElementById("travel-page"),
+            img: document.getElementById("travel-img"),
+            currentDay: document.getElementById("current-day-field"),
+            progressBar: document.getElementById("progress-bar"),
+            walkBtn: document.getElementById("walk-btn")
+        },
+        campPage: {
+            element: document.getElementById("camp-page"),
+            img: document.getElementById("camp-img")
+        },
+        sleepPage: {
+            element: document.getElementById("sleep-page")
+        },
+        huntPage: {
+            element: document.getElementById("hunt-page")
+        },
+        healPage: {
+            element: document.getElementById("heal-page")
+        },
+        statsPage: {
+            element: document.getElementById("stats-page"),
+            nameField: document.getElementById("name-field"),
+            relationshipField: document.getElementById("relationship-field"),
+            thinkingField: document.getElementById("thinking-field"),
+            healthField: document.getElementById("health-field"),
+            woundsField: document.getElementById("wounds-field"),
+            hungerField: document.getElementById("hunger-field"),
+            fatigueField: document.getElementById("fatigue-field"),
+            itemField: document.getElementById("item-field")
+        },
+        walkingPage: {
+            element: document.getElementById("walking-page"),
+            walkingMessage: document.getElementById("walking-message"),
+            walkingMessageFound: document.getElementById("walking-message-found")
+        },
+        reportPage: {
+            element: document.getElementById("report-page"),
+            reportList: document.getElementById("report-list")
+        },
+        eventPage: {
+            element: document.getElementById("report-page"),
+            img: document.getElementById("event-page-image"),
+            eventBtn: document.getElementById("event-page-back-btn")
+        },
+        gameOverPage: {
+            element: document.getElementById("game-over-page")
+        }
     }
 }
