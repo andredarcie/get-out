b16cb4c7cae8af89c99afc513fb0ac9b83971e1d
andredarcie <andrendarcie@gmail.com>
2020-11-05 01:38:31 -0300

    Updates

--- sw.js
+++ sw.js
@@ -25,6 +25,34 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
+    "url": "1.4f022b7a.jpg",
+    "revision": "29d1ad4532617e122e2f53b09b75fbb1"
+  },
+  {
+    "url": "2.a2c87685.jpg",
+    "revision": "ef18a8e162e940b76c33bbbe75c7adab"
+  },
+  {
+    "url": "3.332a3909.jpg",
+    "revision": "e1e1e4b00e6459d244a6068b681b9bd6"
+  },
+  {
+    "url": "4.88fbf1a9.jpg",
+    "revision": "514d3f2a5fb8fd9998486ded99ecf816"
+  },
+  {
+    "url": "5.92f49589.jpg",
+    "revision": "423b7ef2d072a5cc5abd2c9beda4bc8d"
+  },
+  {
+    "url": "6.28c06797.jpg",
+    "revision": "bec4b3e984e323172649addf293d5e84"
+  },
+  {
+    "url": "7.c19e343f.jpg",
+    "revision": "d8941adb0a020cbb58d44760477adda0"
+  },
+  {
     "url": "camp.2289b279.jpg",
     "revision": "a47d34422e8940a081c35d0c8fe50731"
   },
@@ -62,15 +90,15 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "53a613ee34bd16a6e1128b80fc8440f6"
+    "revision": "8a5aea7f2bdb3aafcf1ef44203779faa"
   },
   {
     "url": "normalize.458d1ead.css",
     "revision": "a3e6ec9214680b9ca6634914d899ef02"
   },
   {
-    "url": "src.22dd5e26.js",
-    "revision": "c6fffe9eb7e5632749784412d6bad65a"
+    "url": "src.e68f3384.js",
+    "revision": "c748066bdaebeb2497f7aad61875b0f1"
   },
   {
     "url": "style.8095b8f6.css",
@@ -78,7 +106,7 @@ self.__precacheManifest = [
   },
   {
     "url": "/",
-    "revision": "14a175c62783726c5ed22ebec1df7cc1"
+    "revision": "280d3ee50f52d53fde4e636af46628d6"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
